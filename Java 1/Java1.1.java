13eb539265499a0914a6c7076727f117eea226fd
Felipe <felipegusmguim@gmail.com>
2025-03-20 13:59:28 -0300

    Atividades de Java (Ainda não completa)

---
+++ Java 1/Java1.1.java
@@ -0,0 +1,28 @@
+
+package java.pkg1.pkg1;
+
+
+public class Java11 {
+
+
+    public static void main(String[] args) {
+      
+            int NumA = 10;
+        int NumB = 2;
+        
+        System.out.println(NumA);
+        System.out.println(NumB);
+        
+        int con1 = NumA + NumB;
+        int con2 = NumA - NumB;
+        int con3 = NumA / NumB;
+        int con4 = NumA * NumB;
+        
+         System.out.println("A Soma do NÃºmeros A e B Ã© "+con1);
+         System.out.println("A SubtraÃ§Ã£o dos NÃºmeros A e B Ã© "+con2);
+         System.out.println("A DivisÃ£o dos NÃºmero A e B Ã© "+con3);
+         System.out.println("A MultipliacaÃ§Ã£o dos nÃºmeros A e B Ã© "+con4);
+                
+    }
+    
+}
---
+++ Java 1/Java1.2.java
@@ -0,0 +1,18 @@
+
+package java.pkg1.pkg2;
+
+public class Java12 {
+    
+    public static void main(String[] args) {
+    int Km = 235;
+    int Litros = 2;
+    
+    System.out.println(Km);
+    System.out.println(Litros);
+    System.out.println("A cada 100 km rodados 2 L de combustível se vão");
+    
+    float con1 = Km / Litros;
+    System.out.println("Nesse Percurso ele gastará "+con1+" Litros de Gasolina");
+    }
+    
+}
